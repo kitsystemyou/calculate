6e0f61f75c2245648f3ce14cd0ca691658e60977
kitsystemyou <kitsystemyou@gmail.com>
2024-03-12 01:09:57 +0900

    implement try 1000 times each k and calculate successs rate

--- solve-80percent/main.py
+++ solve-80percent/main.py
@@ -1,14 +1,6 @@
 import random
 
 
-def roll_dice_100() -> int:
-    return random.randint(1, 100)
-
-
-def challange_k(k: int) -> int:
-    return k
-
-
 def is_full(array: list[bool], k: int) -> bool:
     for i in range(k, len(array) - 1):
         if not array[i]:
@@ -17,15 +9,15 @@ def is_full(array: list[bool], k: int) -> bool:
 
 
 def calulate_dice_result(array: list[bool], k: int) -> list[bool]:
-    dice: int = roll_dice_100()
-    print("dice: ", dice)
+    dice: int = random.randint(1, 100)
+    # print("dice: ", dice)
     dice -= 1
     if dice > k - 1:
-        print("dice is bigger than k")
+        # print("dice is bigger than k")
         array[dice] = True
         return array
     else:
-        print("dice is smoller than k")
+        # print("dice is smoller than k")
         # Change a random false value to true (bigger than k)
         false_indices = [i for i, val in enumerate(array[k:]) if not val]
         if false_indices:
@@ -34,17 +26,30 @@ def calulate_dice_result(array: list[bool], k: int) -> list[bool]:
         return array
 
 
-if __name__ == "__main__":
-    k = 50
+# 1game means 200times dice roll
+def try_1_game(k: int) -> bool:
     array: list[bool] = [False for _ in range(100)]
-    calulate_dice_result(array, 50)
-    for i in range(100):
+    for i in range(200):
         new_array = calulate_dice_result(array, k)
         array = new_array
         del new_array
+    return is_full(array, k)
+
+
+def try_100_games(k: int) -> list[bool]:
+    result: list[bool] = []
+    for i in range(1000):
+        result.append(try_1_game(k))
+    return result
+
+
+if __name__ == "__main__":
+    for k in range(1, 101):
+        print("k = ", k)
+        result_array = try_100_games(k)
+        print("success rate = ", result_array.count(True) / len(result_array))
     print("-- END --")
     # for i, val in enumerate(array):
     #     print(i, val)
-    print(is_full(array, k))
 
 # TODO: 200times, calculate whether 80% or not
